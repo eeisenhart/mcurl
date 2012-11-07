1a7bf2c8d60e71c96b1e013fc4974bfeda0735c1
Michael Haskell <mikehas@gmail.com>
2012-11-06 20:44:22 -0800

    Fixed moodle2/1.1

--- queries/moodle2/1.1_course_adoption_percent_available.sql
+++ queries/moodle2/1.1_course_adoption_percent_available.sql
@@ -3,5 +3,5 @@ SELECT count(*) AS 'made available',
   @TOTAL AS 'total courses', 
   CAST( count(*) / @TOTAL AS DECIMAL(2,2)) AS 'percent available' 
 FROM DB_PREFIXcourse
- WHERE TERM_FIELD like TERM_EXPRESSION
+ WHERE TERM_FIELD like 'TERM_EXPRESSION'
  AND visible = 1
