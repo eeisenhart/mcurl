05c3831b585b0b0d681383954efeac3e7fcd6e75
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2012-11-15 16:33:38 -0800

    Some instance total data

---
+++ templates/moodle2/3.9_sonoma.edu_student_total.sql
@@ -0,0 +1,18 @@
+SELECT 
+    COUNT(DISTINCT (ue.userid)) AS 'Student Total (all terms)'
+FROM
+    DB_PREFIX_user_enrolments ue
+        JOIN
+    DB_PREFIX_enrol e ON (ue.enrolid = e.id)
+        JOIN
+    DB_PREFIX_course c ON (c.id = e.courseid)
+        JOIN
+    DB_PREFIX_context ct ON (ct.instanceid = c.id)
+        JOIN
+    DB_PREFIX_role_assignments ra ON (ra.contextid = ct.id
+        AND ue.userid = ra.userid)
+        JOIN
+    DB_PREFIX_role r ON (ra.roleid = r.id)
+WHERE
+        r.shortname = 'STUDENT_ROLE'
+        AND ct.contextlevel = 50;
