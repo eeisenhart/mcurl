2e78c061fc7072cec4f276aa222d78ebbc556f01
Michael Haskell <mikehas@gmail.com>
2013-02-05 13:29:37 -0800

    Added queries to gather class size information, 6 buckets.

---
+++ templates/moodle2/7.1_number_courses_enrollment_size_1_5.sql
@@ -0,0 +1,12 @@
+SELECT 
+	count(*) AS number_courses_enrollment_size_1_5
+FROM 
+	(SELECT DB_PREFIX_course.shortname, COUNT(DB_PREFIX_role_assignments.id) CC 
+	FROM 
+	DB_PREFIX_role_assignments INNER JOIN DB_PREFIX_context 
+	ON ( DB_PREFIX_role_assignments.contextid = DB_PREFIX_context.id) INNER JOIN DB_PREFIX_course 
+	ON ( DB_PREFIX_context.instanceid = DB_PREFIX_course.id) INNER JOIN DB_PREFIX_role 
+	ON ( DB_PREFIX_role_assignments.roleid = DB_PREFIX_role.id) 
+WHERE DB_PREFIX_course.TERM_FIELD LIKE 'TERM_EXPRESSION' AND DB_PREFIX_role.shortname = 'STUDENT_ROLE' 
+GROUP BY DB_PREFIX_course.shortname 
+HAVING CC between 1 and 5) CNT;
