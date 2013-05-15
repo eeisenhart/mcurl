1ad780c35afa5576c9db36a01b17d66a43897d56
Eric Eisenhart <eric.eisenhart@sonoma.edu>
2013-05-15 16:31:42 -0700

    Another query from Sue Bennett

--- templates/moodle2/sonoma_course_adoption_by_active.sql
+++ templates/moodle2/sonoma_course_adoption_by_active.sql
@@ -3,13 +3,13 @@ SELECT
     COUNT(DEPT_QUERY) as 'Course Count with Activity'
 FROM
     (SELECT
-        DB_PREFIX_course.TERM_FIELD TERM_FIELD, COUNT(DB_PREFIX_log.id) CC
+        c.TERM_FIELD, COUNT(DB_PREFIX_log.id) CC
     FROM
         DB_PREFIX_log
-    INNER JOIN DB_PREFIX_course ON (DB_PREFIX_log.course = DB_PREFIX_course.id)
+    INNER JOIN DB_PREFIX_course c ON (DB_PREFIX_log.course = c.id)
     WHERE
-        DB_PREFIX_course.TERM_FIELD LIKE 'TERM_EXPRESSION'
-    GROUP BY DB_PREFIX_course.TERM_FIELD
+        c.TERM_FIELD LIKE 'TERM_EXPRESSION'
+    GROUP BY c.TERM_FIELD
     HAVING CC > 50) CNT
 GROUP BY
     DEPT_QUERY
